0347e122f7e0db5d2910dfe70a928291a521107f
eugenia <55488624+ivevv@users.noreply.github.com>
2021-09-29 18:24:52 +0300

    Update application.py

--- lab1/draft/application.py
+++ lab1/draft/application.py
@@ -18,42 +18,45 @@ class TApplication:
         a = float(input("a = "))  # check if a =/= 0
         while a == 0:
             print("a =/= 0")
-            print("a = ")
-            a = float(input())
+            # print("a = ")
+            a = float(input("a = "))
         # print("b = ")
         b = float(input("b = "))  # check if b =/= 0
         while b == 0:
             print("b =/= 0")
-            print("b = ")
-            b = float(input())
+            # print("b = ")
+            b = float(input("b = "))
         # print("c = ")
         c = float(input("c = "))  # check if c =/= 0
         while c == 0:
             print("c =/= 0")
-            print("c = ")
-            c = float(input())
+            # print("c = ")
+            c = float(input("c = "))
         poli.set_a(a)
         poli.set_b(b)
         poli.set_c(c)
 
     @staticmethod
-    def get_unknowns(poli: TPolinom):
-        check = poli.get_discriminant()
-        if check:
-            print("x1 = " + repr(poli.get_x1()))
-            print("x2 = " + repr(poli.get_x2()))
+    def solve_equation(poli: TPolinom):
+        poli.get_discriminant()
+        if poli.d >= 0:
+            if poli.d != 0:
+                print("x1 = " + repr(poli.get_x1()))
+                print("x2 = " + repr(poli.get_x2()))
+            else:
+                print("x = " + repr(poli.get_x1()))
         else:
             print("This equation doesn't have a solution!")
 
     @staticmethod
-    def solve_polinom(poli: TPolinom):
+    def calculate_polinom(poli: TPolinom):
         # print("x = ")
         x = float(input("x = "))
         while x == 0:
             print("x =/= 0")
-            print("x = ")
-            x = float(input())
-        print("Your result: " + repr(poli.solve(x)))
+            # print("x = ")
+            x = float(input("x = "))
+        print("Your result: " + repr(poli.calculate_w_x(x)))
 
     @staticmethod
     def print_polinom(poli: TPolinom):
