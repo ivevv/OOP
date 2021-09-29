b1042274fe985d8a7ed3547d5e112e891b02cb51
eugenia <55488624+ivevv@users.noreply.github.com>
2021-09-29 16:57:20 +0300

    fixed some stupid mistakes

--- lab1/draft/polinom.py
+++ lab1/draft/polinom.py
@@ -4,15 +4,9 @@ import math
 # polinom: ax^2 + bx + c = 0
 class TPolinom:
     d = 0.0
-    # a = 0.0
-    # b = 0.0
-    # c = 0.0
-
-    def __init__(self, a, b, c):
-        # might put these as class attributes instead, haven't decided yet
-        self.a = a
-        self.b = b
-        self.c = c
+    a = 0.0
+    b = 0.0
+    c = 0.0
 
     def set_a(self, a):
         self.a = a
@@ -24,18 +18,19 @@ class TPolinom:
         self.c = c
 
     def get_discriminant(self):
-        self.d = math.sqrt(self.b) + 4 * self.a * self.c
+        self.d = math.pow(self.b, 2) - 4 * self.a * self.c
+        # print(self.d)
         if self.d < 0:
             return False
         else:
             return True
 
     def get_x1(self):
-        x1 = (self.d - self.b) / (2 * self.a)
+        x1 = (math.sqrt(self.d) - self.b) / (2 * self.a)
         return x1
 
     def get_x2(self):
-        x2 = (0 - self.d - self.b) / (2 * self.a)
+        x2 = (0 - math.sqrt(self.d) - self.b) / (2 * self.a)
         return x2
 
     # temporary name
