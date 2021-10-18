7ffa16fd6131cc97a0bf29768b2caaaeebf6bf06
chlvek <guno.30pm@gmail.com>
2021-10-18 12:27:41 +0300

    добавила свою версию 1 лабы

---
+++ 1 LAB (ver 2)/polinom.py
@@ -0,0 +1,27 @@
+class TPolinom:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def d(self):
+        return self.b**2-4*self.a*self.c
+
+    def get_roots(self):
+        d = self.d()
+        x1 = (-self.b+d)/2*self.a
+        x2 = (-self.b-d)/2*self.a
+        return x1, x2
+
+    def get_value(self, x):
+        value = self.a * x ** 2 + self.b * x + self.c
+        return value
+
+    def __str__(self):
+        return f'{self.a}x² + {self.b}x + {self.c}'
+
+
+
+
+
+
