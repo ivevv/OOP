1e8d6867a7200be63d6a2178c29ccb7efd64e921
chlvek <guno.30pm@gmail.com>
2021-10-27 22:46:13 +0300

    добавлена проверка

--- 2 lab/complex.py
+++ 2 lab/complex.py
@@ -1,5 +1,20 @@
+from math import sqrt
+from typing import Union
+
+
 class TComplex(object):
-    def __init__(self, real=0, imag=0):  # можно использовать real: int, imag: int (или float)?
+    def __new__(
+        cls,
+        real: Union[int, float] = 0,
+        imag: Union[int, float] = 0,
+    ):
+        return complex(real, imag)
+
+    def __init__(
+        self,
+        real: Union[int, float] = 0,
+        imag: Union[int, float] = 0,
+    ):
         """
         :param real: вещественная часть
         :param imag: мнимая часть компл числа (imaginary part)
@@ -8,25 +23,37 @@ class TComplex(object):
         self.imag = imag
 
     def __sub__(self, other):
+        other = self.check_other(other)
         return TComplex(self.real - other.real, self.imag - other.imag)
-    
+
     # нужно ещё перегрузить pow, добавить метод sqrt
 
+    def __pow__(self, other):
+        other = self.check_other(other)
+
+    def __abs__(self):
+        return sqrt(self.real ** 2 + self.imag ** 2)
+
     def __add__(self, other):
+        other = self.check_other(other)
         return TComplex(self.real + other.real, self.imag + other.imag)
 
     def __mul__(self, other):
+        other = self.check_other(other)
         return TComplex(self.real * other.real - self.imag * other.imag,
                         self.imag * other.real + self.real * other.imag)
 
     def __eq__(self, other):
+        other = self.check_other(other)
         return (self.real == other.real) and (self.imag == other.imag)
 
     # т.к. интересуют только знаки перед всем числом
     def __gt__(self, other):
+        other = self.check_other(other)
         return self.real > other.real
 
     def __lt__(self, other):
+        other = self.check_other(other)
         return self.real < other.real
 
     def __str__(self):
@@ -37,3 +64,9 @@ class TComplex(object):
             s += f' {self.imag}i)'
         return s
 
+    def check_other(self, other):
+        if isinstance(other, int) or isinstance(other, float):
+            other = TComplex(other)
+        if not isinstance(other, TComplex):
+            pass
+        return other
